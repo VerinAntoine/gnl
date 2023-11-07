6ce00dd36f2a1d3c63dbb1087beecffe2999cf8b
Antoine Verin <24255551+VerinAntoine@users.noreply.github.com>
2023-11-07 15:00:12 +0100

    re init

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:24:48 by antoine           #+#    #+#             */
-/*   Updated: 2023/11/07 14:26:30 by antoine          ###   ########.fr       */
+/*   Updated: 2023/11/07 14:32:09 by antoine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER_SIZE 200
 
 char *get_next_line(int fd);
 
