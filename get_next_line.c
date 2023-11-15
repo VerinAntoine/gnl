5a2e172a48120e21e744fd032fef84ad139329e2
Antoine Verin <averin@made-f0Br3s5.clusters.42paris.fr>
2023-11-15 16:51:44 +0100

    quasi bon vu que les invalid read ca existe pas

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,67 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 13:41:59 by averin            #+#    #+#             */
-/*   Updated: 2023/11/15 13:53:40 by averin           ###   ########.fr       */
+/*   Updated: 2023/11/15 16:49:08 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	is_valid(char *line)
+{
+	if (!line)
+		return (FALSE);
+	return (line[ft_strlen(line) - 1] == '\n');
+}
+
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE];
+	char		*process;
+	char		*line;
+
+	line = NULL;
+	// while (!buffer[0] && read_file(fd, (char **) &buffer))
+	while (read_file(fd, (char **) &buffer) && !is_valid(line))
+	{
+		process = process_line(buffer);
+		if (!process)
+			return (NULL); //! line to free
+		if (line)
+			line = ft_strcat(line, process); //! can be null
+		else 
+			line = process;
+	}
+	return (line);
+}
+
+int	read_file(int fd, char **buffer)
+{
+	int	readed;
+
+	if (!buffer[0])
+	{
+		readed = read(fd, buffer, BUFFER_SIZE);
+		if(readed <= 0)
+			return (FALSE);
+	}
+	return (TRUE);
+}
+
+char	*process_line(char *buffer)
+{
+	char	*line;
+	size_t	len;
+	size_t	i;
+
+	len = find_end_line(buffer);
+	line = ft_calloc(len + 1, sizeof(char));
+	if (!line)
+		return (NULL);
+	i = -1;
+	while (++i <= len)
+		line[i] = buffer[i];
+	line[i] = '\0';
+	ft_shift_str(buffer, len + 1, BUFFER_SIZE); //! could ko when len == BUFFER_SIZE
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 13:42:36 by averin            #+#    #+#             */
-/*   Updated: 2023/11/15 13:53:31 by averin           ###   ########.fr       */
+/*   Updated: 2023/11/15 16:42:55 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
+# define FALSE 0
+# define TRUE 1
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 200
 # endif
 
 char	*get_next_line(int fd);
+int		read_file(int fd, char **buffer);
+char	*process_line(char *buffer);
+
+size_t	find_end_line(char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(const char *s);
+char	*ft_strcat(char *s1, char *s2);
+void	ft_shift_str(char *s, size_t shift, size_t len);
 
 #endif
